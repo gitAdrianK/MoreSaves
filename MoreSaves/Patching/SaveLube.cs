8d78a3cc00b5d1ddd5d98fd4f9554bc629b743c0
Adrian K <14051526+gitAdrianK@users.noreply.github.com>
2024-05-08 20:23:12 +0200

    Undo delete patch
    Ofc loading a new game also deletes the old one, duh

--- MoreSaves/Patching/SaveLube.cs
+++ MoreSaves/Patching/SaveLube.cs
@@ -4,7 +4,6 @@ using JumpKing.MiscSystems.Achievements;
 using JumpKing.SaveThread;
 using MoreSaves.Util;
 using System;
-using System.IO;
 using System.Reflection;
 
 namespace MoreSaves.Patching
@@ -46,14 +45,6 @@ namespace MoreSaves.Patching
                 saveGeneralSettings,
                 postfix: savePatch
             );
-
-            MethodInfo deleteSave = saveLube.GetMethod("DeleteSaves");
-            HarmonyMethod deletePatch = new HarmonyMethod(AccessTools.Method(typeof(SaveLube), nameof(DeleteSaves)));
-
-            ModEntry.harmony.Patch(
-                deleteSave,
-                postfix: deletePatch
-            );
         }
 
         public static void CopySavefile()
@@ -63,14 +54,6 @@ namespace MoreSaves.Patching
                 return;
             }
             CopyUtil.CopyOutSaves("auto", ModEntry.saveName);
-        }
-
-        public static void DeleteSaves()
-        {
-            char sep = Path.DirectorySeparatorChar;
-            string directory = $"{ModEntry.dllDirectory}{sep}auto{sep}{ModEntry.saveName}{sep}";
-            Directory.Delete(directory, true);
-            ModEntry.saveName = string.Empty;
         }
     }
 }
