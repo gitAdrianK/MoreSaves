59bfc26381d52fa7a210e7b837b4625aeb83e216
Adrian K <14051526+gitAdrianK@users.noreply.github.com>
2024-05-08 08:43:32 +0200

    Make fields static and init once
    Untested

--- MoreSaves/Nodes/NodeLoadSave.cs
+++ MoreSaves/Nodes/NodeLoadSave.cs
@@ -14,7 +14,8 @@ using System.Reflection;
 namespace MoreSaves.Nodes
 {
     /// <summary>
-    /// Node to copy in a save into the Jump King folder.
+    /// Node to load a save from the mod into Jump King.
+    /// All required fields will be set and the JK menu will reload/update.
     /// Force closes the game.
     /// </summary>
     public class NodeLoadSave : IBTnode
@@ -29,53 +30,87 @@ namespace MoreSaves.Nodes
         private static readonly string INVENTORY = "inventory.inv";
         private static readonly string SETTINGS = "general_settings.set";
 
-        /// <summary>
-        /// Folders that make up the path to the file.
-        /// </summary>
-        private string[] folders;
+        private static bool isInitialized;
+
+        private static JKContentManager contentManager;
+        private static SaveManager saveManager;
+
+        private static Type saveLube;
+        private static Type encryption;
+        private static Type achievementManager;
+
+        private static MethodInfo setCombinedSave;
+        private static MethodInfo setEventFlags;
+        private static MethodInfo setPlayerStats;
+        private static MethodInfo setInventory;
+        private static MethodInfo setGeneralSettings;
+
+        private static MethodInfo loadFile;
+        private static MethodInfo loadCombinedSaveFile;
+        private static MethodInfo loadEventFlags;
+        private static MethodInfo loadPlayerStats;
+        private static MethodInfo loadInventory;
+
+        private static MethodInfo saveProgramStartInitialize;
+        private static MethodInfo saveCombinedSaveFile;
+
+        private static object achievementManagerInstance;
+        private static Traverse achievementManagerTraverse;
+
+        private string directory;
 
         public NodeLoadSave(params string[] folders)
         {
-            this.folders = folders;
+            Initialize();
+            directory = ModEntry.dllDirectory;
+            foreach (string folder in folders)
+            {
+                directory += folder + SEP;
+            }
+        }
+
+        private void Initialize()
+        {
+            if (isInitialized)
+            {
+                return;
+            }
+            // Classes and methods.
+            contentManager = Game1.instance.contentManager;
+            saveManager = SaveManager.instance;
+
+            saveLube = AccessTools.TypeByName("JumpKing.SaveThread.SaveLube");
+            encryption = AccessTools.TypeByName("FileUtil.Encryption.Encryption");
+            achievementManager = AccessTools.TypeByName("JumpKing.MiscSystems.Achievements.AchievementManager");
+
+            setCombinedSave = saveLube.GetMethod("set_CombinedSave");
+            setEventFlags = saveLube.GetMethod("set_eventFlags");
+            setPlayerStats = saveLube.GetMethod("set_PlayerStatsAttemptSnapshot");
+            setInventory = saveLube.GetMethod("set_inventory");
+            setGeneralSettings = saveLube.GetMethod("set_generalSettings");
+
+            loadFile = encryption.GetMethod("LoadFile");
+            loadCombinedSaveFile = loadFile.MakeGenericMethod(typeof(CombinedSaveFile));
+            loadEventFlags = loadFile.MakeGenericMethod(typeof(EventFlagsSave));
+            loadPlayerStats = loadFile.MakeGenericMethod(typeof(PlayerStats));
+            loadInventory = loadFile.MakeGenericMethod(typeof(Inventory));
+
+            saveProgramStartInitialize = saveLube.GetMethod("ProgramStartInitialize");
+            saveCombinedSaveFile = saveLube.GetMethod("SaveCombinedSaveFile");
+
+            achievementManagerInstance = achievementManager.GetField("instance", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+            achievementManagerTraverse = Traverse.Create(achievementManagerInstance);
+
+            isInitialized = true;
         }
 
         protected override BTresult MyRun(TickData p_data)
         {
-            JKContentManager contentManager = Game1.instance.contentManager;
-            SaveManager saveManager = SaveManager.instance;
             try
             {
                 saveManager.StopSaving();
 
-                // Classes and methods.
-                Type saveLube = AccessTools.TypeByName("JumpKing.SaveThread.SaveLube");
-                Type encryption = AccessTools.TypeByName("FileUtil.Encryption.Encryption");
-                Type achievementManager = AccessTools.TypeByName("JumpKing.MiscSystems.Achievements.AchievementManager");
-
-                MethodInfo setCombinedSave = saveLube.GetMethod("set_CombinedSave");
-                MethodInfo setEventFlags = saveLube.GetMethod("set_eventFlags");
-                MethodInfo setPlayerStats = saveLube.GetMethod("set_PlayerStatsAttemptSnapshot");
-                MethodInfo setInventory = saveLube.GetMethod("set_inventory");
-                MethodInfo setGeneralSettings = saveLube.GetMethod("set_generalSettings");
-
-                MethodInfo loadFile = encryption.GetMethod("LoadFile");
-                MethodInfo loadCombinedSaveFile = loadFile.MakeGenericMethod(typeof(CombinedSaveFile));
-                MethodInfo loadEventFlags = loadFile.MakeGenericMethod(typeof(EventFlagsSave));
-                MethodInfo loadPlayerStats = loadFile.MakeGenericMethod(typeof(PlayerStats));
-                MethodInfo loadInventory = loadFile.MakeGenericMethod(typeof(Inventory));
-
-                MethodInfo saveProgramStartInitialize = saveLube.GetMethod("ProgramStartInitialize");
-                MethodInfo saveCombinedSaveFile = saveLube.GetMethod("SaveCombinedSaveFile");
-
-                object achievementManagerInstance = achievementManager.GetField("instance", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
-
                 // Load from dllDirectory
-                string directory = ModEntry.dllDirectory;
-                foreach (string folder in folders)
-                {
-                    directory += folder + SEP;
-                }
-
                 CombinedSaveFile combinedSaveFile = (CombinedSaveFile)loadCombinedSaveFile.Invoke(null, new object[] { $"{directory}{SEP}{SAVES}{SEP}{COMBINED}" });
                 EventFlagsSave eventFlags = (EventFlagsSave)loadEventFlags.Invoke(null, new object[] { $"{directory}{SEP}{SAVES_PERMA}{SEP}{EVENT}" });
                 PlayerStats playerStats = (PlayerStats)loadPlayerStats.Invoke(null, new object[] { $"{directory}{SEP}{SAVES_PERMA}{SEP}{STATS}" });
@@ -97,7 +132,7 @@ namespace MoreSaves.Nodes
                 }
 
                 // Save and set
-                Game1.instance.contentManager.ReinitializeAssets();
+                contentManager.ReinitializeAssets();
 
                 if (root == "Content")
                 {
@@ -118,7 +153,7 @@ namespace MoreSaves.Nodes
                 saveCombinedSaveFile.Invoke(null, null);
                 saveProgramStartInitialize.Invoke(null, null);
 
-                Traverse.Create(achievementManagerInstance).Field("m_snapshot").SetValue(playerStats);
+                achievementManagerTraverse.Field("m_snapshot").SetValue(playerStats);
 
                 EventFlagsSave.Save = eventFlags;
 
