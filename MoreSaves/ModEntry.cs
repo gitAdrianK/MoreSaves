acbbce0925cf67c2b736a292f039801105c19a72
Adrian K <14051526+gitAdrianK@users.noreply.github.com>
2024-05-13 14:51:21 +0200

    Revert last commit.

--- MoreSaves/ModEntry.cs
+++ MoreSaves/ModEntry.cs
@@ -123,5 +123,14 @@ namespace MoreSaves
             saveName = Regex.Replace(saveName, "^[c|C][o|O][m|M]\\d$", $"Save_{saveName}");
             saveName = Regex.Replace(saveName, "^[l|L][p|P][t|T]\\d$", $"Save_{saveName}");
         }
+
+        /// <summary>
+        /// Called by Jump King when the Level Ends
+        /// </summary>
+        [OnLevelEnd]
+        public static void OnLevelEnd()
+        {
+            saveName = string.Empty;
+        }
     }
 }
