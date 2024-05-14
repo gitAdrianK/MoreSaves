f0276c786c8eb8fe4aed2df0474620f66bfcf35e
Adrian K <14051526+gitAdrianK@users.noreply.github.com>
2024-05-14 14:48:49 +0200

    Change how much is patched

--- MoreSaves/Patching/SaveLube.cs
+++ MoreSaves/Patching/SaveLube.cs
@@ -1,6 +1,4 @@
 ﻿using HarmonyLib;
-using JumpKing.MiscEntities.WorldItems.Inventory;
-using JumpKing.MiscSystems.Achievements;
 using JumpKing.SaveThread;
 using MoreSaves.Util;
 using System;
@@ -20,30 +18,10 @@ namespace MoreSaves.Patching
 
             MethodInfo generic = saveLube.GetMethod("Save");
             MethodInfo saveCombinedSaveFile = generic.MakeGenericMethod(typeof(CombinedSaveFile));
-            MethodInfo saveEventFlags = generic.MakeGenericMethod(typeof(EventFlagsSave));
-            MethodInfo savePlayerStats = generic.MakeGenericMethod(typeof(PlayerStats));
-            MethodInfo saveInventory = generic.MakeGenericMethod(typeof(Inventory));
-            MethodInfo saveGeneralSettings = generic.MakeGenericMethod(typeof(GeneralSettings));
             HarmonyMethod savePatch = new HarmonyMethod(AccessTools.Method(typeof(SaveLube), nameof(CopySavefile)));
 
             ModEntry.harmony.Patch(
                 saveCombinedSaveFile,
-                postfix: savePatch
-            );
-            ModEntry.harmony.Patch(
-                saveEventFlags,
-                postfix: savePatch
-            );
-            ModEntry.harmony.Patch(
-                savePlayerStats,
-                postfix: savePatch
-            );
-            ModEntry.harmony.Patch(
-                saveInventory,
-                postfix: savePatch
-            );
-            ModEntry.harmony.Patch(
-                saveGeneralSettings,
                 postfix: savePatch
             );
 
@@ -57,7 +35,13 @@ namespace MoreSaves.Patching
         }
 
         // CONSIDER: It could probably improved that instead of copying ALL files out whenever savelube
-        // saves to instead just copy out the files that are being saved. 
+        // saves to instead just copy out the files that are being saved.
+
+        // Problem with this is that certain files are created/saved before the level has loaded, so the saveName is unset.
+        // Collecting an item which one would assume saves inventory,
+        // instead throws "NotImplementedExceptions". So for now we'll leave it at this.
+        // However combined is saved constantly so lets not patch all.
+
         /// <summary>
         /// Copies the required save files out from the games Saves and SavesPerma folder into a folder in
         /// the auto subfolder of the mod dll with the name of the saveName.
